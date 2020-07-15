8ca1e00cbdb437ca8338e249962ea5ddd12c4343
ThisWorld.Online <ThisWorldOnline1@gmail.com>
2020-07-16 02:13:16 +0300

    Enable HTTP2

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -7,6 +7,8 @@ if [ -n "$LETSENCRYPT_DOMAINS" ]; then
     certbot --nginx -d $LETSENCRYPT_DOMAINS \
             --redirect --hsts \
             --non-interactive --agree-tos --register-unsafely-without-email &&
+    sed -i "s/listen 443 ssl;/listen 443 ssl http2;/" /etc/nginx/nginx.conf &&
+    service nginx reload &&
     while :; do sleep 7d; certbot renew || true; done
   ) > /var/log/certbot.log 2>&1 &
 fi
